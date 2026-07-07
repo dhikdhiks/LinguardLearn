dcade96a4aa88172e0afb82c9fbce4c19f8bb653
dhikdhiks <andhika.august4@smk.belajar.id>
2026-07-07 23:59:14 +0700

    Remove vercel.json, use UI settings

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   outputFileTracingRoot: require('path').join(__dirname, '../../'),
 };
